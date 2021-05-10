9b9af72fdf86035d132801bfd144a93ae22421d4
jhjung <jhjung3072@naver.com>
2021-05-10 09:29:10 +0900

    created entities and using lombok

--- src/main/java/com/vega/springit/SpringitApplication.java
+++ src/main/java/com/vega/springit/SpringitApplication.java
@@ -1,7 +1,11 @@
 package com.vega.springit;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringitApplication {
@@ -9,5 +13,7 @@ public class SpringitApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringitApplication.class, args);
     }
+
+
 
 }
