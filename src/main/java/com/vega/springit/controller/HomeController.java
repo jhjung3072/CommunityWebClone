6c363793d9b8847dbe331b9e1d58a653f31be084
jhjung <jhjung3072@naver.com>
2021-05-12 16:32:30 +0900

    created entities and using lombok

---
+++ src/main/java/com/vega/springit/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.vega.springit.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home(){
+        return "index";
+    }
+}
