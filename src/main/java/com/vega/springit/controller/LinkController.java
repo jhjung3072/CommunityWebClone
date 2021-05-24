ced1d9596ade5286d07e6058c351edae9bac9940
jhjung <jhjung3072@naver.com>
2021-05-24 16:50:44 +0900

    Add a new comment

--- src/main/java/com/vega/springit/controller/LinkController.java
+++ src/main/java/com/vega/springit/controller/LinkController.java
@@ -1,9 +1,12 @@
 package com.vega.springit.controller;
 
+import com.vega.springit.domain.Comment;
 import com.vega.springit.domain.Link;
+import com.vega.springit.repository.CommentRepository;
 import com.vega.springit.repository.LinkRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -20,9 +23,11 @@ public class LinkController {
     private static final Logger logger = LoggerFactory.getLogger(LinkController.class);
 
     private LinkRepository linkRepository;
+    private CommentRepository commentRepository;
 
-    public LinkController(LinkRepository linkRepository) {
+    public LinkController(LinkRepository linkRepository, CommentRepository commentRepository) {
         this.linkRepository = linkRepository;
+        this.commentRepository = commentRepository;
     }
 
     @GetMapping("/")
@@ -35,7 +40,11 @@ public class LinkController {
     public String read(@PathVariable Long id, Model model) {
         Optional<Link> link = linkRepository.findById(id);
         if (link.isPresent()){
-            model.addAttribute("link",link.get());
+            Link currentLink = link.get();
+            Comment comment=new Comment();
+            comment.setLink(currentLink);
+            model.addAttribute("comment",comment);
+            model.addAttribute("link",currentLink);
             model.addAttribute("success",model.containsAttribute("success"));
             return "link/view";
         }else{
@@ -62,6 +71,18 @@ public class LinkController {
             return "redirect:/link/{id}";
         }
 
+    }
+
+    @Secured("ROLE_USER")
+    @PostMapping("/link/comments")
+    public String addComment(@Valid Comment comment, BindingResult bindingResult){
+        if (bindingResult.hasErrors()){
+            logger.info("댓글작성에 실패했습니다.");
+        }else{
+            commentRepository.save(comment);
+            logger.info("댓글을 작성했습니다.");
+        }
+        return "redirect:/link/" + comment.getLink().getId();
     }
 
 }
--- src/main/java/com/vega/springit/domain/Comment.java
+++ src/main/java/com/vega/springit/domain/Comment.java
@@ -1,21 +1,26 @@
 package com.vega.springit.domain;
 
+import com.vega.springit.service.BeanUtil;
 import lombok.*;
+import org.ocpsoft.prettytime.PrettyTime;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.Objects;
 
 @Entity
 @RequiredArgsConstructor
 @Getter
 @Setter
-@ToString
 @NoArgsConstructor
 public class Comment extends Auditable{
 
     @Id
     @GeneratedValue
     private Long id;
+
     @NonNull
     private String body;
 
@@ -23,4 +28,12 @@ public class Comment extends Auditable{
     @NonNull
     private Link link;
 
+    public String getPrettyTime() {
+        PrettyTime pt = BeanUtil.getBean(PrettyTime.class);
+        return pt.format(convertToDateViaInstant(getCreationDate()));
+    }
+
+    private Date convertToDateViaInstant(LocalDateTime dateToConvert) {
+        return java.util.Date.from(dateToConvert.atZone(ZoneId.systemDefault()).toInstant());
+    }
 }
